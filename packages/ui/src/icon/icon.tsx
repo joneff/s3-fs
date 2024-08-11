d1a8f6273e9908df62706d1cd934abe867386ac2
joneff <joneff@gmail.com>
2024-08-11 14:28:13 +0300

    chore add types to icon component

--- packages/ui/src/icon/icon.tsx
+++ packages/ui/src/icon/icon.tsx
@@ -1,13 +1,26 @@
 import './styles/icon.css';
 
-export function Icon({icon}) {
+export type IconProps = {
+    icon?: { viewBox: string, content: string }
+}
+
+export function Icon(props: IconProps & React.HTMLAttributes<HTMLSpanElement>) {
+
+    const {
+        icon
+    } = props;
+
+    const hasIcon = icon !== undefined;
+
     return (
         <>
             <span className="icon">
-                <svg viewBox={icon.viewBox}
-                    dangerouslySetInnerHTML={{__html: icon.content}}
-                >
-                </svg>
+                {hasIcon && (
+                    <svg viewBox={icon.viewBox}
+                        dangerouslySetInnerHTML={{__html: icon.content}}
+                    >
+                    </svg>
+                )}
             </span>
         </>
     );
